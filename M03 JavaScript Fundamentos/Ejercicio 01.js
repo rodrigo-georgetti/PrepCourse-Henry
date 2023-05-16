6a841eb813f985c7ae99e04921ccd144136652a7
rodrigo-georgetti <rodrigogeorgetti.cba@gmail.com>
2023-05-15 23:01:46 -0300

    primer commit

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,7 +5,7 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = null;
+const nuevoString = "Hola";
 
 // Crea una variable de tipo number.
 const nuevoNumero = null;
